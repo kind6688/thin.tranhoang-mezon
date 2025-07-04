3f0dbe4a0586868fb125ec25fbbc7f362b951319
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-07-04 11:55:16 +0700

    ADD.

--- libs/ui/src/lib/Icons/icons.tsx
+++ libs/ui/src/lib/Icons/icons.tsx
@@ -1114,22 +1114,17 @@ export const CanvasIcon: React.FC<IconProps> = ({ defaultSize = 'w-5 h-5' }) => 
 	);
 };
 
-export const TopicIcon: React.FC<IconProps> = ({ isWhite, defaultSize = 'w-5 h-5' }) => {
+export const TopicIcon: React.FC<IconProps> = ({ defaultSize = 'w-5 h-5', className = '' }) => {
 	return (
 		<svg
 			width="24"
 			height="24"
 			fill="none"
-			x="0px"
-			y="0px"
 			xmlns="http://www.w3.org/2000/svg"
 			viewBox="0 0 24 24"
-			className={`hover:text-black dark:hover:text-white ${defaultSize} ${isWhite ? 'dark:text-white text-black' : 'dark:text-[#B5BAC1] text-colorTextLightMode'}`}
+			className={`${defaultSize} ${className}`}
 		>
-			<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-			<g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
 			<g id="SVGRepo_iconCarrier">
-				{' '}
 				<path
 					fillRule="evenodd"
 					clipRule="evenodd"
@@ -1138,11 +1133,12 @@ export const TopicIcon: React.FC<IconProps> = ({ isWhite, defaultSize = 'w-5 h-5
 					strokeWidth="1.5"
 					strokeLinecap="round"
 					strokeLinejoin="round"
-				></path>{' '}
+				/>
 				<path
 					d="M9.5 13.25C9.08579 13.25 8.75 13.5858 8.75 14C8.75 14.4142 9.08579 14.75 9.5 14.75V13.25ZM13.5 14.75C13.9142 14.75 14.25 14.4142 14.25 14C14.25 13.5858 13.9142 13.25 13.5 13.25V14.75ZM9.5 10.25C9.08579 10.25 8.75 10.5858 8.75 11C8.75 11.4142 9.08579 11.75 9.5 11.75V10.25ZM15.5 11.75C15.9142 11.75 16.25 11.4142 16.25 11C16.25 10.5858 15.9142 10.25 15.5 10.25V11.75ZM9.5 14.75H13.5V13.25H9.5V14.75ZM9.5 11.75H15.5V10.25H9.5V11.75Z"
-					fill="currentColor"
-				></path>{' '}
+					stroke="currentColor"
+					strokeWidth="1.5"
+				/>
 			</g>
 		</svg>
 	);
